d0eff2adaf5f22815dd3ace32adce15d66461069
lloyd18gh <lloyd18gg@gmail.com>
2020-09-30 03:15:37 +0800

    .

--- x86/x64-part1.sh
+++ x86/x64-part1.sh
@@ -1,8 +1,9 @@
 # 添加主题/插件
-git clone https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
-git clone -b master https://github.com/vernesong/OpenClash.git package/OpenClash
-#git clone https://github.com/lloyd18/luci-app-npc.git package/luci-app-npc
-#git clone https://github.com/lloyd18/npc.git package/npc
+#git clone https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
+git clone https://github.com/garypang13/luci-theme-edge.git package/luci-theme-edge
+git clone https://github.com/vernesong/OpenClash.git package/OpenClash
+git clone https://github.com/lloyd18/luci-app-npc.git package/luci-app-npc
+git clone https://github.com/lloyd18/npc.git package/npc
 git clone https://github.com/lloyd18/automount.git package/automount
 
 # 解决冲突
