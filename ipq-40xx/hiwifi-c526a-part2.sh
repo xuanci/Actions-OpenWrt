3f2f688d9dfe879a4041db11d9cbf54df4e6aacd
supsupsuperstar <43098395+supsupsuperstar@users.noreply.github.com>
2021-01-21 05:33:54 +0800

    Update hiwifi-c526a-part2.sh

--- ipq-40xx/hiwifi-c526a-part2.sh
+++ ipq-40xx/hiwifi-c526a-part2.sh
@@ -64,8 +64,6 @@ cat >> .config <<EOF
 CONFIG_PACKAGE_luci-app-vssr=y
 CONFIG_PACKAGE_luci-app-vlmcsd=y
 CONFIG_PACKAGE_luci-app-qosv4=y
-CONFIG_PACKAGE_luci-app-eqos=y
-CONFIG_PACKAGE_luci-app-qos-gargoyle=y
 EOF
 
 # 常用软件 默认已启用
