a33dab9f42d3bcb51f7d79a9bcf84ea5b20770f4
supsupsuperstar <43098395+supsupsuperstar@users.noreply.github.com>
2021-01-21 20:40:09 +0800

    Update hiwifi-c526a-part2.sh

--- ipq-40xx/hiwifi-c526a-part2.sh
+++ ipq-40xx/hiwifi-c526a-part2.sh
@@ -61,11 +61,11 @@ CONFIG_PACKAGE_ffprobe=y
 EOF
 
 cat >> .config <<EOF
-CONFIG_PACKAGE_luci-app-passwall=y
+CONFIG_PACKAGE_luci-app-openclash=y
 CONFIG_PACKAGE_luci-app-ssr-plus=y
 CONFIG_PACKAGE_luci-app-vlmcsd=y
 CONFIG_PACKAGE_luci-app-qosv4=y
-CONFIG_PACKAGE_luci-app-uugamebooster=y
+
 EOF
 
 # 常用软件 默认已启用
